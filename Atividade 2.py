1754991c8619f134dc3b6d868349e08d4b30fa2f
renansantz <renanclaudino09@gmail.com>
2026-05-02 22:23:05 -0300

    Atividade 2.py

---
+++ Atividade 2.py
@@ -0,0 +1,8 @@
+while True:
+    senha = 1597
+    a = int(input("Digite um valor: "))
+    if senha == a:
+        print("Senha correta")
+        break
+    else:
+        print("Senha incorreta, tente novamente")
